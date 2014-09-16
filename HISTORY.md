b929fd8c81a5f52b70d423b307083528bb06d431
cweihan <cwei@wizcorp.jp>
2014-09-16 14:10:59 +0900

    Fixed default to object, added history and bumped version

---
+++ HISTORY.md
@@ -0,0 +1,49 @@
+# Release history
+
+## 0.1.11
+
+Allow parsing index in save and load
+
+## 0.1.10
+
+Bump csvparser version to v0.1.18.
+
+## 0.1.9
+
+Bump csvparser version to v0.1.17.
+
+## 0.1.8
+
+Bump csvparser version to v0.1.16.
+
+## 0.1.7
+
+Added onLoad hooks to create dynamic tests based on staticData
+
+## 0.1.6
+
+We lost the ability to parse values, but we got it back.
+
+## 0.1.5
+
+Bump csvparser version to v0.1.15.
+
+## 0.1.4
+
+Bump csvparser version to v0.1.14.
+
+## 0.1.3
+
+Bump csvparser version to v0.1.13.
+
+## 0.1.2
+
+Fixed a bug with parsers not being available.
+
+## 0.1.1
+
+Changes parsely to not handle component requiring.
+
+## 0.1.0
+
+First version.
--- index.js
+++ index.js
@@ -9,7 +9,7 @@ exports.buildQuery = function (dataSources) {
 
 	for (var i = 0; i < dataSources.length; i += 1) {
 		var dataSource = dataSources[i];
-		query[dataSource] = { topic: dataSource, index: [] };
+		query[dataSource] = { topic: dataSource, index: {} };
 	}
 
 	return query;
@@ -42,7 +42,7 @@ exports.addDataListeners = function (archivist, dataSources) {
 
 function getLoader(archivist, name, importer) {
 	return function (cb) {
-		archivist.get(importer.topic || name, importer.index || [], { optional: true }, function (error, data) {
+		archivist.get(importer.topic || name, importer.index || {}, { optional: true }, function (error, data) {
 			if (error) {
 				console.log('error loading', name);
 			}
@@ -60,7 +60,7 @@ function getSaver(archivist, name, importer) {
 			data = transform(data, exports.staticData);
 		}
 
-		archivist.set(importer.topic || name, importer.index || [], data);
+		archivist.set(importer.topic || name, importer.index || {}, data);
 		archivist.distribute(cb);
 	};
 }
